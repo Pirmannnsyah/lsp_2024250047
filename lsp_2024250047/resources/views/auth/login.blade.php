4c6ec5c4509ffa75a7c8254d2250f2b4122d95e1
Firman <100285601+Pirmannnsyah@users.noreply.github.com>
2024-08-08 01:11:01 +0700

    desain login regis

--- lsp_2024250047/resources/views/auth/login.blade.php
+++ lsp_2024250047/resources/views/auth/login.blade.php
@@ -7,8 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
+    @extends('layout.navbar')
 
-    <div class="w-full max-w-md bg-white shadow-md rounded-lg p-8">
+    @section('title', 'Pengumuman')
+
+    @section('content')
+
+    <div class="w-full max-w-md bg-white shadow-md rounded-lg p-16">
         <!-- Session Status -->
         @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
@@ -22,7 +27,7 @@
             <!-- Email Address -->
             <div>
                 <label for="email" class="block text-sm font-medium text-gray-700">{{ __('Email') }}</label>
-                <input id="email" class="block mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
+                <input id="email" class="p-2 block mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
                 @error('email')
                     <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
                 @enderror
@@ -31,7 +36,7 @@
             <!-- Password -->
             <div class="mt-4">
                 <label for="password" class="block text-sm font-medium text-gray-700">{{ __('Password') }}</label>
-                <input id="password" class="block mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" type="password" name="password" required autocomplete="current-password">
+                <input id="password" class="p-2 block mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" type="password" name="password" required autocomplete="current-password">
                 @error('password')
                     <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
                 @enderror
@@ -45,9 +50,9 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-between mt-4">
+            <div class="flex items-center justify-between mt-4 gap-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-blue-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
@@ -62,6 +67,6 @@
             </div>
         </form>
     </div>
-
+    @endsection
 </body>
 </html>
--- lsp_2024250047/resources/views/auth/register.blade.php
+++ lsp_2024250047/resources/views/auth/register.blade.php
@@ -7,6 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
+    @extends('layout.navbar')
+
+    @section('title', 'Pengumuman')
+
+    @section('content')
 
     <div class="w-full max-w-md bg-white shadow-md rounded-lg p-8">
         <form method="POST" action="{{ route('user.store') }}">
@@ -16,39 +21,43 @@
                 <!-- Nama Depan -->
                 <div>
                     <label for="first_name" class="block text-sm font-medium text-gray-700">Nama Depan</label>
-                    <input id="first_name" type="text" name="first_name" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
+                    <input id="first_name" type="text" name="first_name" class="p-2 mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
                 </div>
 
                 <!-- Nama Belakang -->
                 <div>
                     <label for="last_name" class="block text-sm font-medium text-gray-700">Nama Belakang</label>
-                    <input id="last_name" type="text" name="last_name" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
+                    <input id="last_name" type="text" name="last_name" class="p-2 mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
                 </div>
             </div>
 
             <!-- NIK -->
             <div class="mb-6">
                 <label for="nik" class="block text-sm font-medium text-gray-700">NIK</label>
-                <input id="nik" type="text" name="nik" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
+                <input id="nik" type="text" name="nik" class="p-2 mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
             </div>
 
             <!-- Email -->
             <div class="mb-6">
                 <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                <input id="email" type="email" name="email" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
+                <input id="email" type="email" name="email" class="p-2 mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
             </div>
 
             <!-- Phone -->
             <div class="mb-6">
                 <label for="phone" class="block text-sm font-medium text-gray-700">Telepon</label>
-                <input id="phone" type="text" name="phone" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
+                <input id="phone" type="text" name="phone" class="p-2 mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
             </div>
 
             <!-- Password -->
             <div class="mb-6">
                 <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                <input id="password" type="password" name="password" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
+                <input id="password" type="password" name="password" class="p-2 mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
             </div>
+
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                    {{ __('Already have an account ? log in') }}
+                </a>
 
             <div class="flex justify-end">
                 <button type="submit" class="bg-indigo-600 text-white px-4 py-2 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -57,6 +66,6 @@
             </div>
         </form>
     </div>
-
+    @endsection
 </body>
 </html>
